a24eb56f77b218316a7fde032753c8e12df96964
Cherkesky <guy@cherkesky.com>
2020-08-16 15:43:34 -0500

    comments

--- word_count.exs
+++ word_count.exs
@@ -1,6 +1,6 @@
 filename = IO.gets("File to count the words from: ") |> String.trim()
 words = File.read!(filename)
-        |> String.split(~r{(\\n|[^\w'])+})
+        |> String.split(~r{(\\n|[^\w'])+}) #regex to split on any word or new line
         |> Enum.filter(fn x -> x != "" end)
 IO.inspect words
 words
